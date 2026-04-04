b835e87f30e5daa2768f73c0ccb2ccee90e90ec3
suhayla88 <suhailamukhtar65@gmail.com>
2026-04-04 15:40:01 +0300

    I learned ch3_functions

--- ch3_functions/script.js
+++ ch3_functions/script.js
@@ -1,1 +1,80 @@
-v
+// function whatgirlslike() {
+
+// console.log("Girls like kindness.");
+// console.log("Girls like honesty.");
+// console.log("Girls like respect.");
+// console.log("Girls like good communication.");
+// console.log("Girls like thoughtful surprises.");
+// console.log("Girls like feeling appreciated.");
+// console.log("Girls like supportive friends.");
+// console.log("Girls like laughter and humor.");
+// console.log("Girls like confidence.");
+// console.log("Girls like creativity.");
+// console.log("Girls like meaningful conversations.");
+// console.log("Girls like loyalty.");
+// console.log("Girls like cute pets.");
+// console.log("Girls like music.");
+// console.log("Girls like relaxing time.");
+// console.log("Girls like adventures.");
+// console.log("Girls like compliments.");
+// console.log("Girls like fashion.");
+// console.log("Girls like good food.");
+// console.log("Girls like positive vibes.");
+// }
+
+// function Whatboyslike() { 
+// console.log("Boys like games.");
+// console.log("Boys like competition.");
+// console.log("Boys like sports.");
+// console.log("Boys like adventure.");
+// console.log("Boys like humor.");
+// console.log("Boys like loyalty.");
+// console.log("Boys like good food.");
+// console.log("Boys like hanging out with friends.");
+// console.log("Boys like music.");
+// console.log("Boys like technology.");
+// console.log("Boys like freedom.");
+// console.log("Boys like challenges.");
+// console.log("Boys like respect.");
+// console.log("Boys like support.");
+// console.log("Boys like learning new skills.");
+// console.log("Boys like movies.");
+// console.log("Boys like traveling.");
+// console.log("Boys like cool gadgets.");
+// console.log("Boys like fun activities.");
+// console.log("Boys like relaxing time.");
+// }
+ 
+
+// whatgirlslike(); 
+
+
+// function multiply(a, b) { 
+//     return a * b;
+// }
+
+
+
+// var result = multiply(5, 10);
+// var result2 = multiply(3, 7);
+
+// console.log(result);
+// console.log(result2);
+
+function add (a, b) {
+    return a + b;
+}
+
+var sum1 = add(55, 170);
+var sum2 = add(38, 67);
+
+console.log(sum1);
+console.log(sum2);
+
+
+var add = function (a,b) {
+    return a + b;
+}
+
+var sum3 = add(12, 34);
+console.log(sum3);
