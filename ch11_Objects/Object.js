862ae3ec317db252e7141b4585203e0ae9af7c68
suhayla88 <suhailamukhtar65@gmail.com>
2026-05-05 03:41:35 +0300

    I'm learning ch11..

---
+++ ch11_Objects/Object.js
@@ -0,0 +1,23 @@
+// let person = {
+//     name: "Suhayla Mukhtar",
+//     age: 21,
+//     City: "Sweden"
+
+// }
+
+// let vehticle = {
+//     brand: "Toyota", 
+//     model: "Camry",
+//     year: 2020,
+//     color: "blue",
+//     owner: person
+// } 
+
+// console.log(person.name);
+// console.log(vehticle.owner.name);
+
+
+let animal = new Object()
+animal.type = "Dog"
+animal.breed = "Golden Retriever"
+console.log(animal) 
