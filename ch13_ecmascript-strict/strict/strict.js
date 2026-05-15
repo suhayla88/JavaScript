d90bf52dbe4d9cae9a97c6a14c710636e4a3c72f
suhayla88 <suhailamukhtar65@gmail.com>
2026-05-15 19:48:41 +0300

    I learned ch13_ecmascript-strict

---
+++ ch13_ecmascript-strict/strict/strict.js
@@ -0,0 +1,11 @@
+// This code is the old way of declaring varables in JavaScript. And it wouldn't work cuase it's in strict mode.
+// "use strict"; //this will not work, iy will tell u to use the new way of declaring variables, wich is 'let' or 'const' instead of 'var'.
+// firstName = "Suhayla";
+// console.log(firstName);
+
+// but 'let' was added to know that this varaible is new and created the first time. 
+"use strict" //This will work, because we are using 'let' to declare the variable.
+let firstName = "Suhayla";
+console.log(firstName);
+
+//So what 'use strict/ strict mode' means don't use the old way of declaring variables, use the new way of declaring variables.
